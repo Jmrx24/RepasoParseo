ad2d1f0fbf03738566da28570b3523222a0d3b6d
Jmrx24 <Jmrx24@gmail.com>
2019-12-04 18:55:37 +0100

    meto Conexion

---
+++ RepasoParseo/src/repaso/Conexion.java
@@ -0,0 +1,41 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Conexion {
+
+	static String usuario = "root";
+	static String passwd = "";
+	static String url="jdbc:mysql://localhost:3306/repaso?serverTimezone=UTC";
+	static String driver="com.mysql.cj.jdbc.Driver";
+	static Connection conex;
+
+	
+	public static void Acceder() {
+		
+		try {
+			 Class.forName(driver);
+			 conex=DriverManager.getConnection(url, usuario,passwd);
+			 System.out.println("Conexión realizada a la base de datos con éxito.");
+		} catch (Exception e) {
+			 System.out.println(e.getLocalizedMessage());
+		}
+	}
+	
+	 public static Connection getConnection() {
+	        return conex;
+	    }
+
+	public static void insertarDatos() 
+	{
+		
+	}
+	 
+	
+	 
+	
+}
--- RepasoParseo/src/repaso/Parseo.java
+++ RepasoParseo/src/repaso/Parseo.java
@@ -43,6 +43,7 @@ public class Parseo {
 				lista.add(t);
 			}
 			String html=CrearHtml.pintar(lista);
+			
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
